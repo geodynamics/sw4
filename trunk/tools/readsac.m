bb25bbea7f5462fc134934f18217b24194ff0c3d
petersson1 <petersson1@836cf65c-fe5a-4b60-ab5c-b1c9cd11b388>
2012-08-31 21:00:05 +0000

    Improved the readsac.m script to also read KZTIME, i.e., reference datum
    git-svn-id: file:///g/g0/ines/cvsTOsvn/sbp4w.svn@84 836cf65c-fe5a-4b60-ab5c-b1c9cd11b388

--- trunk/tools/readsac.m
+++ trunk/tools/readsac.m
@@ -3,7 +3,7 @@
 %
 %   Read SAC receiever data.
 %
-%     [u,dt,lat,lon,t0] = readsac( fname, format )
+%     [u, dt, lat, lon, b, e, npts, year, jday, hour, min, sec, msec] = readsac( fname, format )
 %
 %          Input: fname  - Name of SAC file
 %                 format - Little endian ('l') or big endian ('b')
@@ -11,10 +11,19 @@
 %
 %          Output: u        - The data component on SAC file
 %                  dt       - Uniform time step for u
-%                  lat, lon - Latitude and longitude position of
+%                  stalat, stalon - Latitude and longitude position of
 %                             the SAC station.
+%                  b        - begin time relative reference datum
+%                  e        - end time relative reference datum
+%                  npts     - Number of elements in u
+%                  year       - reference datum
+%                  Julian day - reference datum
+%                  hour       - reference datum
+%                  minute     - reference datum
+%                  second     - reference datum
+%                  micro sec  - reference datum
 %
-   function [u,dt,lat,lon,t0] = readsac( fname, format )
+   function  [u, dt, lat, lon, b, e, npts, year, jday, hour, min, sec, msec] = readsac( fname, format )
   if nargin < 2 
      format = 'l';
   end;
@@ -37,20 +46,24 @@
      evdepth = fread(fid,1,'float32');
      fseek(fid,4*31,0);
 % integers from offset 70
-     fseek(fid,4*6,0);
+     year = fread(fid,1,'int32');
+     jday = fread(fid,1,'int32');
+     hour = fread(fid,1,'int32');
+     min = fread(fid,1,'int32');
+     sec = fread(fid,1,'int32');
+     msec = fread(fid,1,'int32');
      nvhdr = fread(fid,1,'int32');
      fseek(fid,4*2,0);
      npts=fread(fid,1,'int32');
      fseek(fid,78*4,0);
+% output reference time stamp
+disp(['Year = ' num2str(year) ' Julian Day = ' num2str(jday) ' Hour = ' num2str(hour) ' Min = ' num2str(min) ' Sec = ' num2str(sec) ' Micro Sec = ' num2str(msec) ]);
 % output required header data
 disp(['Begin time (B) = ' num2str(t0) ' End time (E) = ' num2str(t1) ' Station lat lon = ' num2str(lat) ' ' num2str(lon) ' nvhdr = ' num2str(nvhdr) ' npts = ' num2str(npts)]);
 % read time series
      u=fread(fid,npts,'float32');
+% copy begin and end times
+     b = t0;
+     e = t1;
      fclose(fid);
-%    dt=fread(fid,1,'float32')
-%    fseek(fid,78*4,0);
-%    npts=fread(fid,1,'int')
-%    fseek(fid,78*4,0);
-%    u=fread(fid,npts,'float32');
-%    fclose(fid);
   end
