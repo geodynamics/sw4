d9b29f1da38eb262ebdd1efe8c1d26635c75ca11
sjogreen2 <sjogreen2@836cf65c-fe5a-4b60-ab5c-b1c9cd11b388>
2012-11-16 21:05:37 +0000

    Added functions for new image format
    git-svn-id: file:///g/g0/ines/cvsTOsvn/sbp4w.svn@107 836cf65c-fe5a-4b60-ab5c-b1c9cd11b388

---
+++ trunk/tools/getimagemodestr.m
@@ -0,0 +1,78 @@
+%
+% GETIMAGEMODESTR
+%
+%   Translate an integer image mode number to the corresponding mode string.
+%
+%           str=getimagemodestr(mode)
+%                Input: mode - Integer (currently 0 to 32)
+%                Output: str - Name of mode.
+function str=getimagemodestr(mode)
+if mode == 0
+   str='none';
+elseif mode == 1
+   str='ux';
+elseif mode == 2
+   str = 'uy';
+elseif mode == 3
+   str = 'uz';
+elseif mode == 4
+   str = 'rho';
+elseif mode == 5
+   str = 'lambda';
+elseif mode == 6
+   str = 'mu';
+elseif mode == 7
+   str = 'p';
+elseif mode == 8
+   str = 's';
+elseif mode == 9
+   str = 'uxexact';
+elseif mode == 10
+   str = 'uyexact';
+elseif mode == 11
+   str = 'uzexact';
+elseif mode == 12
+   str = 'div';
+elseif mode == 13
+   str = 'curlmag';
+elseif mode == 14
+   str = 'divdt';
+elseif mode == 15
+   str = 'curlmagdt';
+elseif mode == 16
+   str = 'lat';
+elseif mode == 17
+   str = 'lon';
+elseif mode == 18
+   str = 'topo';
+elseif mode == 19
+   str = 'gridx';
+elseif mode == 20
+   str = 'gridy';
+elseif mode == 21
+   str = 'gridz';
+elseif mode == 22
+   str = 'uxerr';
+elseif mode == 23
+   str = 'uyerr';
+elseif mode == 24
+   str = 'uzerr';
+elseif mode == 25
+   str = 'magdudt';
+elseif mode == 26
+   str = 'hmagdudt';
+elseif mode == 27
+   str = 'hmaxdudt';
+elseif mode == 28
+   str = 'vmaxdudt';
+elseif mode == 29
+   str = 'mag';
+elseif mode == 30
+   str = 'hmag';
+elseif mode == 31
+   str = 'hmax';
+elseif mode == 32
+   str = 'vmax';
+else
+  str = 'unknown';
+end;
---
+++ trunk/tools/readimage.m
@@ -0,0 +1,82 @@
+%
+% READIMAGE
+%
+%    Read image produced by sw4 on format with the *.sw4img extension.
+%    This script will not read the older WPP image format.
+%
+%         [im,x,y,z,t]=readimage( imfile, pnr, verbose )
+%
+%                Input: imfile  - Image file name
+%                       pnr     - Patch number, if more than one grid is used.
+%                       verbose - Set to 1 to display file header information.
+%                Output: im      - The image, as a 2D array.
+%                        x, y, z - The spatial coordinates of the image, one of these is a scalar.
+%                        t       - Simulation time at which the image was output.
+function [im,x,y,z,t]=readimage( imfile, pnr, verbose )
+if nargin < 3
+   verbose= 0;
+end;
+if nargin < 2
+   pnr = 1;
+end;
+
+fd=fopen(imfile,'r');
+if fd ~= -1 
+  % Read header
+   prec    =fread(fd,1,'int');
+   npatches=fread(fd,1,'int');
+   t       =fread(fd,1,'double');
+   plane   =fread(fd,1,'int');
+   coord   =fread(fd,1,'double');
+   mode    =fread(fd,1,'int');
+   gridinfo=fread(fd,1,'int');
+   timecreated=fread(fd,[1 25],'uchar');
+   mstr=getimagemodestr(mode);
+  % Display header
+   if verbose == 1
+      disp(['Found:  prec  = ' num2str(prec)  ' t= ' num2str(t) ' plane= ' num2str(plane)]);   
+      disp(['        coord = ' num2str(coord) ' mode= ' mstr ' npatches= ' num2str(npatches) ]);
+      disp(['        gridinfo = ' num2str(gridinfo) ]); 
+     disp(['    file created ' timecreated(1:24)]);
+   end;
+   for p=1:npatches
+      h(p) = fread(fd,1,'double');
+      zmin(p) = fread(fd,1,'double');
+      ib(p) = fread(fd,1,'int');
+      ni(p) = fread(fd,1,'int');
+      jb(p) = fread(fd,1,'int');
+      nj(p) = fread(fd,1,'int');
+      if verbose == 1
+         disp(['    patch nr ' num2str(p) ' has h = ' num2str(h(p)) ' zmin = ' num2str(zmin(p))]);
+      end;
+   end;
+  % Read data
+  if pnr <= npatches
+     for p=1:pnr-1
+	fseek(fd,(ni(p)-ib(p)+1)*(nj(p)-jb(p)+1)*prec,'cof');
+     end;
+     if prec == 4
+        im = fread(fd,[ni(pnr)-ib(pnr)+1 nj(pnr)-jb(pnr)+1],'float');
+     else
+        im = fread(fd,[ni(pnr)-ib(pnr)+1 nj(pnr)-jb(pnr)+1],'double');
+     end;
+     fclose(fd);
+     if plane == 0 
+        x = coord;
+        y = h(pnr)*((ib(pnr):ni(pnr))-1);
+        z = h(pnr)*((jb(pnr):nj(pnr))-1);
+     elseif plane == 1 
+        x = h(pnr)*((ib(pnr):ni(pnr))-1);
+        y = coord;
+        z = h(pnr)*((jb(pnr):nj(pnr))-1);
+     elseif plane == 2
+        x = h(pnr)*((ib(pnr):ni(pnr))-1);
+        y = h(pnr)*((jb(pnr):nj(pnr))-1);
+        z = coord;
+     end;
+  else
+     disp(['Error: number of patches on file ' num2str(npatches) ' is smaller than input pnr = ' num2str(pnr)]);
+  end;
+else
+   disp(['Error: could not open file ' imfile ]);
+end;
