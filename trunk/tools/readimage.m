9bf1c017ec413e408ed95157c6727eab3ea3e568
petersson1 <petersson1@836cf65c-fe5a-4b60-ab5c-b1c9cd11b388>
2013-03-01 19:30:50 +0000

    Improving the readimage script for curvilinear grids
    git-svn-id: file:///g/g0/ines/cvsTOsvn/sbp4w.svn@145 836cf65c-fe5a-4b60-ab5c-b1c9cd11b388

--- trunk/tools/readimage.m
+++ trunk/tools/readimage.m
@@ -34,7 +34,7 @@ if fd ~= -1
    timecreated=fread(fd,[1 25],'uchar');
    timestring=num2str(timecreated,'%c');
    mstr=getimagemodestr(mode);
-  % Display header
+% Display header
    if verbose == 1
       disp(['Found:  prec  = ' num2str(prec)  ' t= ' num2str(t) ' plane= ' num2str(plane)]);   
       disp(['        coord = ' num2str(coord) ' mode= ' mstr ' npatches= ' num2str(npatches) ]);
@@ -52,7 +52,7 @@ if fd ~= -1
          disp(['    patch nr ' num2str(p) ' has h = ' num2str(h(p)) ' zmin = ' num2str(zmin(p))]);
       end;
    end;
-  % Read data
+% Read data
   readz = 0;
   if pnr <= npatches
      for p=1:pnr-1
@@ -63,12 +63,15 @@ if fd ~= -1
      else
         im0 = fread(fd,[ni(pnr)-ib(pnr)+1 nj(pnr)-jb(pnr)+1],'double');
      end;
+% read grid z-coordinates
      if (pnr == npatches) && (gridinfo == 1 )
         if prec == 4
-           z = fread(fd,[ni(pnr)-ib(pnr)+1 nj(pnr)-jb(pnr)+1],'float');
+           z0 = fread(fd,[ni(pnr)-ib(pnr)+1 nj(pnr)-jb(pnr)+1],'float');
         else
-           z = fread(fd,[ni(pnr)-ib(pnr)+1 nj(pnr)-jb(pnr)+1],'double');
+           z0 = fread(fd,[ni(pnr)-ib(pnr)+1 nj(pnr)-jb(pnr)+1],'double');
         end;
+% transpose z0 so it gets the same dimensions as im
+        z = z0';
         readz = 1;
      end;
 % transpose im0 and return result in im
@@ -76,15 +79,31 @@ if fd ~= -1
      fclose(fd);
      if plane == 0 
         x = coord;
-        y = h(pnr)*((ib(pnr):ni(pnr))-1);
         if readz == 0 
+           y = h(pnr)*((ib(pnr):ni(pnr))-1);
            z = zmin(pnr) + h(pnr)*((jb(pnr):nj(pnr))-1);
+        else
+          n1 = size(im,1);
+          n2 = size(im,2);
+% make y two-dimensional (same size as z and im)
+          y = zeros(n1,n2);
+          for i=1:n1
+            y(i,:) = h(pnr)*((ib(pnr):ni(pnr))-1);
+          end
         end;
      elseif plane == 1 
-        x = h(pnr)*((ib(pnr):ni(pnr))-1);
         y = coord;
         if readz == 0 
+           x = h(pnr)*((ib(pnr):ni(pnr))-1);
            z = zmin(pnr) + h(pnr)*((jb(pnr):nj(pnr))-1);
+        else
+          n1 = size(im,1);
+          n2 = size(im,2);
+% make x two-dimensional (same size as z and im)
+          x = zeros(n1,n2);
+          for i=1:n1
+            x(i,:) = h(pnr)*((ib(pnr):ni(pnr))-1);
+          end
         end;
      elseif plane == 2
         x = h(pnr)*((ib(pnr):ni(pnr))-1);
