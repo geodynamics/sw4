b5fa74a523497b7f2e9d9b2d7ce09e9e32f6778f
Bjorn Sjogreen <sjogreen2@llnl.gov>
2017-01-10 13:15:59 -0800

    changed loop order in curvilinear supergrid routines

--- sw4lite/src/ew-cfromfort.C
+++ sw4lite/src/ew-cfromfort.C
@@ -843,11 +843,11 @@ void EW::addsgd6fort( int ifirst, int ilast, int jfirst, int jlast,
 #pragma omp parallel for
       for( int k=kfirst+3; k <= klast-3 ; k++ )
 	 for( int j=jfirst+3; j <= jlast-3 ; j++ )
-#pragma simd
-#pragma ivdep
 	    for( int i=ifirst+3; i <= ilast-3 ; i++ )
 	    {
 	       float_sw4 birho=0.5*beta/rho(i,j,k);
+#pragma simd
+#pragma ivdep
 	       for( int c=0 ; c < 3 ; c++ )
 	       {
 		 up(c,i,j,k) += birho*( 
@@ -1042,6 +1042,7 @@ void EW::addsgd6fort_indrev( int ifirst, int ilast, int jfirst, int jlast,
       const size_t ni = ilast-ifirst+1;
       const size_t nij = ni*(jlast-jfirst+1);
       const size_t npts = nij*(klast-kfirst+1);
+      for( int c=0 ; c < 3 ; c++ )
 #pragma omp parallel for
       for( int k=kfirst+3; k <= klast-3 ; k++ )
 	 for( int j=jfirst+3; j <= jlast-3 ; j++ )
@@ -1050,7 +1051,6 @@ void EW::addsgd6fort_indrev( int ifirst, int ilast, int jfirst, int jlast,
 	    for( int i=ifirst+3; i <= ilast-3 ; i++ )
 	    {
 	       float_sw4 birho=0.5*beta/rho(i,j,k);
-	       for( int c=0 ; c < 3 ; c++ )
 	       {
 		 up(c,i,j,k) += birho*( 
        strx(i)*coy(j)*coz(k)*(
@@ -1140,11 +1140,11 @@ void EW::addsgd4cfort( int ifirst, int ilast, int jfirst, int jlast,
 #pragma omp parallel for
       for( int k=kfirst+2; k <= klast-2 ; k++ )
 	 for( int j=jfirst+2; j <= jlast-2 ; j++ )
-#pragma simd
-#pragma ivdep
 	    for( int i=ifirst+2; i <= ilast-2 ; i++ )
 	    {
 	       float_sw4 irhoj=beta/(rho(i,j,k)*jac(i,j,k));
+#pragma simd
+#pragma ivdep
 	       for( int c=0 ; c < 3 ; c++ )
 	       {
 		  up(c,i,j,k) -= irhoj*( 
@@ -1218,11 +1218,11 @@ void EW::addsgd6cfort( int ifirst, int ilast, int jfirst, int jlast,
 #pragma omp parallel for
       for( int k=kfirst+3; k <= klast-3 ; k++ )
 	 for( int j=jfirst+3; j <= jlast-3 ; j++ )
-#pragma simd
-#pragma ivdep
 	    for( int i=ifirst+3; i <= ilast-3 ; i++ )
 	    {
 	       float_sw4 birho=0.5*beta/(rho(i,j,k)*jac(i,j,k));
+#pragma simd
+#pragma ivdep
 	       for( int c=0 ; c < 3 ; c++ )
 	       {
 		 up(c,i,j,k) += birho*( 
@@ -1295,6 +1295,7 @@ void EW::addsgd4cfort_indrev( int ifirst, int ilast, int jfirst, int jlast,
       const size_t ni   =     (ilast-ifirst+1);
       const size_t nij  =  ni*(jlast-jfirst+1);
       const size_t npts = nij*(klast-kfirst+1);
+      for( int c=0 ; c < 3 ; c++ )
 #pragma omp parallel for
       for( int k=kfirst+2; k <= klast-2 ; k++ )
 	 for( int j=jfirst+2; j <= jlast-2 ; j++ )
@@ -1303,7 +1304,6 @@ void EW::addsgd4cfort_indrev( int ifirst, int ilast, int jfirst, int jlast,
 	    for( int i=ifirst+2; i <= ilast-2 ; i++ )
 	    {
 	       float_sw4 irhoj=beta/(rho(i,j,k)*jac(i,j,k));
-	       for( int c=0 ; c < 3 ; c++ )
 	       {
 		  up(c,i,j,k) -= irhoj*( 
 		  // x-differences
@@ -1373,6 +1373,7 @@ void EW::addsgd6cfort_indrev(  int ifirst, int ilast, int jfirst, int jlast,
       const size_t ni = ilast-ifirst+1;
       const size_t nij = ni*(jlast-jfirst+1);
       const size_t npts = nij*(klast-kfirst+1);
+	       for( int c=0 ; c < 3 ; c++ )
 #pragma omp parallel for
       for( int k=kfirst+3; k <= klast-3 ; k++ )
 	 for( int j=jfirst+3; j <= jlast-3 ; j++ )
@@ -1381,7 +1382,6 @@ void EW::addsgd6cfort_indrev(  int ifirst, int ilast, int jfirst, int jlast,
 	    for( int i=ifirst+3; i <= ilast-3 ; i++ )
 	    {
 	       float_sw4 birho=0.5*beta/(rho(i,j,k)*jac(i,j,k));
-	       for( int c=0 ; c < 3 ; c++ )
 	       {
 		 up(c,i,j,k) += birho*( 
        strx(i)*coy(j)*(
