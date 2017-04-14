020b80c1b932f01aad4a78ab2bb861e53a6ab2d3
Ramesh Pankajakshan <pankajakshan1@llnl.gov>
2017-04-14 11:10:58 -0700

    Commit to add policies.h

---
+++ sw4lite/src/policies.h
@@ -0,0 +1,20 @@
+#ifdef CUDA_CODE
+typedef NestedPolicy<ExecList<cuda_threadblock_x_exec<4>,cuda_threadblock_y_exec<4>,
+  cuda_threadblock_z_exec<16>>>
+  EXEC_CARTBC;
+
+#define REDUCE_BLOCK_SIZE 1024
+typedef RAJA::cuda_exec<REDUCE_BLOCK_SIZE> EXEC;
+
+typedef cuda_reduce<REDUCE_BLOCK_SIZE> REDUCE_POLICY;
+#define SYNC_DEVICE cudaDeviceSynchronize();
+#else
+typedef NestedPolicy<ExecList<omp_parallel_for_exec,omp_parallel_for_exec,
+  omp_parallel_for_exec>>
+  EXEC_CARTBC;
+
+typedef RAJA::omp_parallel_for_exec EXEC;
+typedef omp_reduce REDUCE_POLICY;
+#define  SYNC_DEVICE
+#endif
+
