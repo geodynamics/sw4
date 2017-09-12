b100045d0e6fd218777e6e4d23c3b47991703f21
Nils Anders Petersson <apetersson1@bitbucket.org>
2017-09-12 15:14:25 -0700

    Improving 2 octave scripts

--- tools/plotsac.m
+++ tools/plotsac.m
@@ -43,6 +43,7 @@ end
 h=plot(t+tshift,ux,colorstring);
 %set(h,'LineWidth',2.0)
 set(gca,'FontSize',20)
+legend("East","location","southwest");
 axis tight;
 
 % north component
@@ -53,6 +54,7 @@ end
 h=plot(t+tshift,uy,colorstring);
 %set(h,'LineWidth',2.0)
 set(gca,'FontSize',20)
+legend("North","location","southwest");
 axis tight;
 
 % up component
@@ -63,4 +65,5 @@ end
 h=plot(t+tshift,uz,colorstring);
 %set(h,'LineWidth',2.0)
 set(gca,'FontSize',20)
+legend("Up","location","southwest");
 axis tight;
