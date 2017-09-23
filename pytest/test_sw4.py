7868eef3fb8289ff62b485e4c9a05cf62596797f
Anders Petersson <petersson1@llnl.gov>
2017-09-23 11:58:25 -0700

    Adding # OMP threads as argument to test_sw4.py

--- pytest/test_sw4.py
+++ pytest/test_sw4.py
@@ -77,22 +77,27 @@ def compare_one_line(base_file_name, test_file_name, errTol, absErrLimit, lineNu
     return success
 
 #------------------------------------------------
-def guess_mpi_cmd(mpi_tasks, verbose):
+def guess_mpi_cmd(mpi_tasks, omp_threads, verbose):
     if verbose: print('os.uname=', os.uname())
     node_name = os.uname()[1]
     if verbose: print('node_name=', node_name)
     sys_name = os.uname()[0]
     if verbose: print('sys_name=', sys_name)
 
+
     if 'quartz' in node_name:
-        if mpi_tasks<=0: mpi_tasks = 36
-        mpirun_cmd="srun -ppdebug -n " + str(mpi_tasks)
+        if omp_threads<=0: omp_threads=2;
+        if mpi_tasks<=0: mpi_tasks = int(36/omp_threads)
+        mpirun_cmd="srun -ppdebug -n " + str(mpi_tasks) + " -c " + str(omp_threads)
     elif 'cab' in node_name:
-        if mpi_tasks<=0: mpi_tasks = 16
-        mpirun_cmd="srun -ppdebug -n " + str(mpi_tasks)
+        if omp_threads<=0: omp_threads=2;
+        if mpi_tasks<=0: mpi_tasks = int(16/omp_threads)
+        mpirun_cmd="srun -ppdebug -n " + str(mpi_tasks) + " -c " + str(omp_threads)
     elif 'nid' in node_name: # the cori knl nodes are called nid
-        if mpi_tasks<=0: mpi_tasks = 64
-        mpirun_cmd="srun -n " + str(mpi_tasks) + " -c 4 --cpu_bind=cores"
+        if omp_threads<=0: omp_threads=4;
+        if mpi_tasks<=0: mpi_tasks = int(64/omp_threads) # use 64 hardware cores per node
+        sw_threads = 4*omp_threads # Cori uses hyperthreading by default
+        mpirun_cmd="srun --cpu_bind=cores -n " + str(mpi_tasks) + " -c " + str(sw_threads)
     elif 'fourier' in node_name:
         if mpi_tasks<=0: mpi_tasks = 4
         mpirun_cmd="mpirun -np " + str(mpi_tasks)
@@ -105,12 +110,10 @@ def guess_mpi_cmd(mpi_tasks, verbose):
         if mpi_tasks<=0: mpi_tasks = 1
         mpirun_cmd="mpirun -np " + str(mpi_tasks)
 
-    if verbose: print('mpirun_cmd = ', mpirun_cmd)
-
     return mpirun_cmd
 
 #------------------------------------------------
-def main_test(sw4_exe_dir="optimize", testing_level=0, mpi_tasks=0, verbose=False):
+def main_test(sw4_exe_dir="optimize", testing_level=0, mpi_tasks=0, omp_threads=0, verbose=False):
     assert sys.version_info >= (3,2) # named tuples in Python version >=3.3
     sep = '/'
     pytest_dir = os.getcwd()
@@ -146,10 +149,13 @@ def main_test(sw4_exe_dir="optimize", testing_level=0, mpi_tasks=0, verbose=Fals
         return False
 
     # guess the mpi run command from the uname info
-    mpirun_cmd=guess_mpi_cmd(mpi_tasks, verbose)
+    mpirun_cmd=guess_mpi_cmd(mpi_tasks, omp_threads, verbose)
 
     sw4_mpi_run = mpirun_cmd + ' ' + sw4_exe
-    #print('sw4_mpi_run = ', sw4_mpi_run)
+    if (omp_threads>0):
+        sw4_mpi_run = 'export OMP_NUM_THREADS=' + str(omp_threads) + '; ' + sw4_mpi_run
+
+    if verbose: print('sw4_mpi_run = ', sw4_mpi_run)
 
     num_test=0
     num_pass=0
@@ -199,10 +205,10 @@ def main_test(sw4_exe_dir="optimize", testing_level=0, mpi_tasks=0, verbose=Fals
     
             # pipe stdout and stderr to a temporary file
             run_cmd = sw4_mpi_run + ' ' + sw4_input_file + ' >& ' + sw4_stdout_file
-
             # run sw4
             run_dir = os.getcwd()
             #print('Running sw4 from directory:', run_dir)
+# assign OMP_NUM_THREADS
             status = os.system(run_cmd)
             if status!=0:
                 print('ERROR: Test', test_case, ': sw4 returned non-zero exit status=', status, 'aborting test')
@@ -241,12 +247,14 @@ if __name__ == "__main__":
     testing_level=0
     verbose=False
     mpi_tasks=0 # machine dependent default
+    omp_threads=0 #no threading by default
 
     parser=argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
     parser.add_argument("-l", "--level", type=int, choices=[0, 1, 2], 
                         help="testing level")
     parser.add_argument("-m", "--mpitasks", type=int, help="number of mpi tasks")
+    parser.add_argument("-t", "--ompthreads", type=int, help="number of omp threads per task")
     parser.add_argument("-d", "--sw4_exe_dir", help="name of directory for sw4 executable", default="optimize")
     args = parser.parse_args()
     if args.verbose:
@@ -258,10 +266,13 @@ if __name__ == "__main__":
     if args.mpitasks:
         #print("MPI-tasks specified=", args.mpitasks)
         if args.mpitasks > 0: mpi_tasks=args.mpitasks
+    if args.ompthreads:
+        #print("OMP-threads specified=", args.ompthreads)
+        if args.ompthreads > 0: omp_threads=args.ompthreads
     if args.sw4_exe_dir:
         #print("sw4_exe_dir specified=", args.sw4_exe_dir)
         sw4_exe_dir=args.sw4_exe_dir
 
-    if not main_test(sw4_exe_dir, testing_level, mpi_tasks, verbose):
+    if not main_test(sw4_exe_dir, testing_level, mpi_tasks, omp_threads, verbose):
         print("test_sw4 was unsuccessful")
 
