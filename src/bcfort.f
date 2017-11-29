44e2c0d26fccad2baa8aedc56859f8d6a8cb0235
Nils Anders Petersson <apetersson1@bitbucket.org>
2017-11-29 15:15:34 -0800

    Updated config file for vulcan

--- src/bcfort.f
+++ src/bcfort.f
@@ -1,35 +1,35 @@
-!  SW4 LICENSE
-! # ----------------------------------------------------------------------
-! # SW4 - Seismic Waves, 4th order
-! # ----------------------------------------------------------------------
-! # Copyright (c) 2013, Lawrence Livermore National Security, LLC. 
-! # Produced at the Lawrence Livermore National Laboratory. 
-! # 
-! # Written by:
-! # N. Anders Petersson (petersson1@llnl.gov)
-! # Bjorn Sjogreen      (sjogreen2@llnl.gov)
-! # 
-! # LLNL-CODE-643337 
-! # 
-! # All rights reserved. 
-! # 
-! # This file is part of SW4, Version: 1.0
-! # 
-! # Please also read LICENCE.txt, which contains "Our Notice and GNU General Public License"
-! # 
-! # This program is free software; you can redistribute it and/or modify
-! # it under the terms of the GNU General Public License (as published by
-! # the Free Software Foundation) version 2, dated June 1991. 
-! # 
-! # This program is distributed in the hope that it will be useful, but
-! # WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
-! # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
-! # conditions of the GNU General Public License for more details. 
-! # 
-! # You should have received a copy of the GNU General Public License
-! # along with this program; if not, write to the Free Software
-! # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA 
-c-----------------------------------------------------------------------
+c  SW4 LICENSE
+c # ----------------------------------------------------------------------
+c # SW4 - Seismic Waves, 4th order
+c # ----------------------------------------------------------------------
+c # Copyright (c) 2013, Lawrence Livermore National Security, LLC. 
+c # Produced at the Lawrence Livermore National Laboratory. 
+c # 
+c # Written by:
+c # N. Anders Petersson (petersson1@llnl.gov)
+c # Bjorn Sjogreen      (sjogreen2@llnl.gov)
+c # 
+c # LLNL-CODE-643337 
+c # 
+c # All rights reserved. 
+c # 
+c # This file is part of SW4, Version: 1.0
+c # 
+c # Please also read LICENCE.txt, which contains "Our Notice and GNU General Public License"
+c # 
+c # This program is free software; you can redistribute it and/or modify
+c # it under the terms of the GNU General Public License (as published by
+c # the Free Software Foundation) version 2, dated June 1991. 
+c # 
+c # This program is distributed in the hope that it will be useful, but
+c # WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+c # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+c # conditions of the GNU General Public License for more details. 
+c # 
+c # You should have received a copy of the GNU General Public License
+c # along with this program; if not, write to the Free Software
+c # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA 
+c----------------------------------------------------------------------
       subroutine bcfort( ifirst, ilast, jfirst, jlast, kfirst, klast, 
      +     wind, nx, ny, nz,
      +     u, h, bccnd, sbop, mu, la, t,
