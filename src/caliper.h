0e20975d8d56855240e60c84f0d34f296eea7283
Ramesh Pankajakshan <pankajakshan1@llnl.gov>
2018-03-16 17:27:53 -0700

    Added a missing include file for caliper.

---
+++ src/caliper.h
@@ -0,0 +1,33 @@
+#ifndef __SW4_CALIPER_ANNOTATIONS__
+#define __SW4_CALIPER_ANNOTATIONS__
+
+#if defined(ENABLE_CALIPER)
+
+#include <caliper/cali.h>
+
+
+
+#define SW4_MARK_FUNCTION CALI_CXX_MARK_FUNCTION
+
+#define SW4_MARK_BEGIN(tag) CALI_MARK_BEGIN((tag))
+
+#define SW4_MARK_END(tag) CALI_MARK_END((tag))
+
+
+
+
+#else
+// Stubs for removing Caliper dependencies
+
+
+
+#define SW4_MARK_FUNCTION 
+
+#define SW4_MARK_BEGIN(tag)
+
+#define SW4_MARK_END(tag)
+
+
+#endif
+
+#endif
