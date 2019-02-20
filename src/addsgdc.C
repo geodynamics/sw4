4866de3734463ed0927b3b87c361ffbb7fcd7ad6
Ramesh Pankajakshan <pankajakshan1@llnl.gov>
2019-02-20 14:37:24 -0800

    Changes to INSTALL.txt. Changed policy in addsgdc.C

--- src/addsgdc.C
+++ src/addsgdc.C
@@ -130,11 +130,11 @@ using ADDSGD_POL_ASYNC_NOTUSED = RAJA::KernelPolicy<
 using ADDSGD_POL_ASYNC = RAJA::KernelPolicy<
   RAJA::statement::CudaKernelFixedAsync<256,
       RAJA::statement::Tile<1, RAJA::statement::tile_fixed<4>, RAJA::cuda_block_y_loop,
-        RAJA::statement::Tile<3, RAJA::statement::tile_fixed<16>, RAJA::cuda_block_x_loop,
-			      RAJA::statement::Tile<2, RAJA::statement::tile_fixed<4>, RAJA::cuda_block_z_loop,
-          RAJA::statement::For<1, RAJA::cuda_thread_y_direct,
-            RAJA::statement::For<3, RAJA::cuda_thread_x_direct,
-				 RAJA::statement::For<2, RAJA::cuda_thread_z_direct,
+        RAJA::statement::Tile<3, RAJA::statement::tile_fixed<64>, RAJA::cuda_block_x_loop,
+			      RAJA::statement::Tile<2, RAJA::statement::tile_fixed<1>, RAJA::cuda_block_z_loop,
+          RAJA::statement::For<1, RAJA::cuda_thread_y_loop,
+            RAJA::statement::For<3, RAJA::cuda_thread_x_loop,
+				 RAJA::statement::For<2, RAJA::cuda_thread_z_loop,
 						      RAJA::statement::For<0, RAJA::seq_exec,
 									   RAJA::statement::Lambda<0> >>>>>>>>>;
 
