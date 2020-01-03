76450a4d2e2d2106724cfb9394fa76dadab2ed4f
lu zhang <luzhang@lus-MacBook-Pro.local>
2020-01-02 19:12:46 -0600

    update interpolation and restriction operator

--- dev/src/condition_number/Plot_sol.m
+++ dev/src/condition_number/Plot_sol.m
@@ -2,7 +2,7 @@
 
 close all
 
-N6 = load('N6_g.txt');
+N6 = load('N6.txt');
 n1_c = N6(1);
 n2_c = N6(2);
 n3_c = N6(3);
@@ -10,28 +10,70 @@ n1_f = N6(4);
 n2_f = N6(5);
 n3_f = N6(6);
 
-X1c = load('X1c_g.txt'); 
-X2c = load('X2c_g.txt'); 
-X3c = load('X3c_g.txt'); X3c = reshape(X3c,n1_c,n2_c,n3_c);
+X1c = load('X1c.txt'); 
+X2c = load('X2c.txt'); 
+X3c = load('X3c.txt'); X3c = reshape(X3c,n1_c,n2_c,n3_c);
 
-X1f = load('X1f_g.txt'); 
-X2f = load('X2f_g.txt'); 
-X3f = load('X3f_g.txt'); X3f = reshape(X3f,n1_f,n2_f,n3_f);
+X1f = load('X1f.txt'); 
+X2f = load('X2f.txt'); 
+X3f = load('X3f.txt'); X3f = reshape(X3f,n1_f,n2_f,n3_f);
 
-err_f = load('err_f_g.txt'); err_f = reshape(err_f,n1_f,n2_f,n3_f);
-err_c = load('err_c_g.txt'); err_c = reshape(err_c,n1_c,n2_c,n3_c);
+% this is used to genrate the 3d mesh
+[x2c,x1c] = meshgrid(X1c,X2c);
+[x2f,x1f] = meshgrid(X1f,X2f);
+figure(1)
+for i = 1:n3_c
+    h4 = mesh(x1c,x2c,X3c(:,:,i));
+	set(h4,'EdgeColor','b','linewidth',1)
+	hold on
+end
+
+z = zeros(1,n3_c);
+for k = 1:n2_c
+    y = ones(1,n3_c)*X2c(k);
+    for i = 1:n1_c
+        x = ones(1,n3_c)*X1c(i);
+        for j = 1:n3_c
+            z(j) = X3c(i,k,j);
+        end 
+        hold on
+        plot3(x,y,z,'b','linewidth',1)
+    end 
+end
+
+for i = 1:n3_f
+    h4 = mesh(x1f,x2f,X3f(:,:,i));
+	set(h4,'EdgeColor','r','linewidth',1)
+	hold on
+end
+
+z = zeros(1,n3_f);
+for k = 1:n2_f
+    y = ones(1,n3_f)*X2f(k);
+    for i = 1:n1_f
+        x = ones(1,n3_f)*X1f(i);
+        for j = 1:n3_f
+            z(j) = X3f(i,k,j);
+        end 
+        hold on
+        plot3(x,y,z,'r','linewidth',1)
+    end 
+end
+
+grid off
+set(gca,'fontsize',12)
+xlabel('x','fontsize',12)
+ylabel('y','fontsize',12)
+zlabel('z','fontsize',12)
 
 % this is used to generate 2d mesh (fixed one direction and calculate another two)
-if (1 == 0)
 for jj = 1 
     Xci = zeros(n1_c,n3_c);
     Xcj = zeros(n1_c,n3_c);
-    errc = zeros(n1_c,n3_c);
     for j = 1:n3_c
         for i = 1:n1_c
             Xci(i,j) = X1c(i);
             Xcj(i,j) = X3c(i,jj,j);
-		    errc(i,j) = err_c(i,jj,j);
 	    end 
     end
     Xfi = zeros(n1_f,n3_f);
@@ -41,65 +83,39 @@ for jj = 1
         for i = 1:n1_f
 	        Xfi(i,j) = X1f(i);
 	        Xfj(i,j) = X3f(i,2*jj-1,j);
-		    errf(i,j) = err_f(i,2*jj-1,j);
 	    end 
     end
-    figure(jj), h1=mesh(Xci,Xcj,Xci*0),view(2)
-    hold on, h2=mesh(Xfi,Xfj,Xfi*0), view(2)
-	%set(h1,'EdgeColor','b','FaceColor','b','MarkerEdgecolor','b','MarkerFacecolor','b')
+    figure(2), h1=mesh(Xci,Xcj,Xci*0,'linewidth',1); view(2);
+    set(gca,'fontsize',12)
+    hold on, h2=mesh(Xfi,Xfj,Xfi*0,'linewidth',1); view(2);
+    plot3(X1c,X3f(1:2:end,1,3),X1c(1)*ones(1,n1_c),'b.','MarkerSize',12);view(2)
+    set(gca,'fontsize',12)
 	set(h1,'EdgeColor','b')
 	set(h2,'EdgeColor','r')
     axis equal
-    xlabel('x')
-	ylabel('z')
-	title('y = 0')
-	print -depsc mesh_y0.eps
-    %figure(jj+n1_c)
-    %mesh(Xci,Xcj,errc)
-    %hold on
-    %mesh(Xfi,Xfj,errf)
+    xlabel('x','fontsize',12)
+	ylabel('z','fontsize',12)
+	%title('y = 0','fontsize',12)
+    axis off
+	%print -depsc mesh_y0.eps
 end 
-end %if
-% this is used to genrate the 3d mesh
-if (1 == 0)
-[x1c,x2c] = meshgrid(X1c,X2c);
-[x1f,x2f] = meshgrid(X1f,X2f);
-figure(1)
-for i = 1:n3_c
-    h3 = mesh(x1c,x2c,X3c(:,:,i));
-	set(h3,'EdgeColor','b')
-	hold on
-end
-for i = 1:n3_f
-    h4 = mesh(x1f,x2f,X3f(:,:,i));
-	set(h4,'EdgeColor','r')
-	hold on
-end 
-grid on
-xlabel('x')
-ylabel('y')
-zlabel('z')
-title('physical domain')
-print -depsc domain.eps
-end % if
-xc = [0,2,4,6,8];
-yc = [0,2,4,6,8];
-xf = [0,1,2,3,4,5,6,7,8];
-yf = [8,9,10,11,12,13,14,15,16];
+
+figure(3)
+xc = (0:2:48)*0.5;
+yc = xc;
+xf = (0:48)*0.5;
+yf = (48:96)*0.5;
 xcg = xc;
-ycg = [10,10,10,10,10];
+ycg = 50*ones(1,25)*0.5;
 [xxc,yyc] = meshgrid(xc,yc);
-[xxcg,yycg] = meshgrid(xc,ycg)
+[xxcg,yycg] = meshgrid(xc,ycg);
 [xxf,yyf] = meshgrid(xf,yf);
-h6 = mesh(xxf,yyf,0*xxf),view(2)
+h6 = mesh(xxf,yyf,0*xxf,'linewidth',1);view(2)
 set(h6,'EdgeColor','r')
 hold on
-h5 = mesh(xxc,yyc,0*yyc),view(2)
+h5 = mesh(xxc,yyc,0*yyc,'linewidth',1);view(2)
 set(h5,'EdgeColor','b')
-plot(xcg,ycg,'b*')
-%h6 = mesh(xxf,yyf,0*xxf),view(2)
-%set(h6,'EdgeColor','r')
+plot(xcg,ycg,'b.','MarkerSize',8)
+axis equal
 grid off
 axis off
-print -depsc mr.eps
-
