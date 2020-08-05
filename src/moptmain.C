0a14cdc8f96a96f2d132f07858946e3b537d9ad0
Wei Liu <liu56@oslic2.llnl.gov>
2020-08-05 10:42:32 -0700

    Revert "comment out traveltime computation"
    This reverts commit 374e1e4a1e174cbf52730ce53b7e8a72d3337ba4.

--- src/moptmain.C
+++ src/moptmain.C
@@ -1290,12 +1290,9 @@ int main(int argc, char **argv)
 	   get_source_pars( nspar, xspar, xs );
 
 // Initialize the material parameters
-      mp->get_parameters(nmpard,xm,nmpars,&xs[nspar],simulation.mRho,simulation.mMu,simulation.mLambda );
-
-
-// solveTT
-//       mp->get_base_parameters(nmpard,xm,nmpars,&xs[nspar],simulation.mRho,simulation.mMu,simulation.mLambda );
-//       simulation.solveTT( GlobalSources[0], GlobalTimeSeries[0], xs, nmpars, 0, myRank);
+       mp->get_base_parameters(nmpard,xm,nmpars,&xs[nspar],simulation.mRho,simulation.mMu,simulation.mLambda );
+	
+       simulation.solveTT( GlobalSources[0], GlobalTimeSeries[0], xs, nmpars, 0, myRank);
 
    //           string parname = simulation.getOutputPath() + "mtrlpar-init.bin";
            string parname = mopt->m_path + "mtrlpar-init.bin";
