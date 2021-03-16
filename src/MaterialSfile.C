2d862b5ce3062646d832c4ac02ab66ce7fc9dd9e
Ramesh Pankajakshan <pankajakshan1@llnl.gov>
2021-03-15 21:22:11 -0700

    Fixed bug causing failed HDF tests. Now passes HF, HMR3 and L2

--- src/MaterialSfile.C
+++ src/MaterialSfile.C
@@ -316,14 +316,14 @@ void MaterialSfile::set_material_properties(std::vector<Sarray> &rho,
   delete[] jst;
 
   mEW->communicate_arrays(rho);
-  mEW->communicate_arrays(cs);
-  mEW->communicate_arrays(cp);
+  mEW->communicate_host_arrays(cs);
+  mEW->communicate_host_arrays(cp);
   mEW->material_ic(rho);
   mEW->material_ic(cs);
   mEW->material_ic(cp);
   if (use_q) {
-    mEW->communicate_arrays(xis);
-    mEW->communicate_arrays(xip);
+    mEW->communicate_host_arrays(xis);
+    mEW->communicate_host_arrays(xip);
     mEW->material_ic(xis);
     mEW->material_ic(xip);
   }
