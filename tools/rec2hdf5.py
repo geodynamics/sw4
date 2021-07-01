6e8cd7afcb164cee8d558fdbec1002074f57fed4
Houjun Tang <htang4@lbl.gov>
2021-07-01 08:58:19 -0700

    Update conversion script

--- tools/rec2hdf5.py
+++ tools/rec2hdf5.py
@@ -18,6 +18,7 @@ xyz=np.array([0.0,0.0,-1.0])
 
 i = 0
 is_nsew = 0
+is_depth = 1
 for line in lines:
     # line = line.lower()
     if line.startswith("rec") or line.startswith("sac") :
@@ -58,6 +59,8 @@ for line in lines:
             kv = pairs.split('=')
             if kv[0] == "depth" or kv[0] == "topodepth" or kv[0] == "z":
                 xyz[2] = float(kv[1])
+                if kv[0] == "z":
+                    is_depth = 0;
             elif kv[0] == "x" :
                 xyz[0] = float(kv[1])
             elif kv[0] == "y" :
@@ -79,8 +82,11 @@ for line in lines:
             dset = grp.create_dataset('STX,STY,STZ', (3,), dtype='f8')
         else:
             dset = grp.create_dataset('STLA,STLO,STDP', (3,), dtype='f8')
-
         dset[:] = xyz
+
+        if is_depth == 0:
+            dset = grp.create_dataset('USEZVALUE', (1,), dtype='i4')
+            dset[0] = 1
 
 dset = outfile.create_dataset('DOWNSAMPLE', (1,), dtype='i4')
 dset[0] = downsample
