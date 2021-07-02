51e18c56686eb1ab89bd0231aec739e2e485ffca
Ramesh Pankajakshan <pankajakshan1@llnl.gov>
2021-07-01 19:12:33 -0700

    Started on adding Cmake support

---
+++ host-configs/rzansel.cmake
@@ -0,0 +1,34 @@
+
+set (ENABLE_MPI ON CACHE BOOL "" FORCE)
+set (ENABLE_CUDA ON CACHE BOOL "" FORCE)
+set (ENABLE_RAJA ON CACHE BOOL "" FORCE)
+
+
+set (CUDA_ARCH "sm_70" CACHE STRING "" FORCE)
+set (MPI_HOME $ENV{MPI_ROOT})
+
+set(MPI_CXX_COMPILER "${MPI_HOME}/bin/mpic++" CACHE PATH "")
+
+set(GCC_VERSION "gcc-8.3.1" CACHE STRING "")
+set(GCC_HOME "/usr/tce/packages/gcc/${GCC_VERSION}")
+
+set(CMAKE_C_COMPILER   "${GCC_HOME}/bin/gcc" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "${GCC_HOME}/bin/g++" CACHE PATH "")
+set(BLT_CXX_STD "c++11" CACHE STRING "")
+
+#------------------------------------------------------------------------------
+# CUDA support
+#------------------------------------------------------------------------------
+
+
+set(CUDA_TOOLKIT_ROOT_DIR "$ENV{CUDA_HOME}" CACHE PATH "")
+set(CMAKE_CUDA_COMPILER "${CUDA_TOOLKIT_ROOT_DIR}/bin/nvcc" CACHE PATH "")
+set(CMAKE_CUDA_HOST_COMPILER "${CMAKE_CXX_COMPILER}" CACHE PATH "")
+
+#set(_cuda_arch "sm_70")
+set(CMAKE_CUDA_FLAGS "-lineinfo -restrict --expt-extended-lambda " CACHE STRING "")
+
+set(CUDA_SEPARABLE_COMPILATION ON CACHE BOOL "" )
+
+# nvcc does not like gtest's 'pthreads' flag
+set(gtest_disable_pthreads ON CACHE BOOL "")
--- src/Sarray.C
+++ src/Sarray.C
@@ -31,7 +31,7 @@
 // # along with this program; if not, write to the Free Software
 // # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA
 #include <fcntl.h>
-#include <foralls.h>
+#include "foralls.h"
 #include <unistd.h>
 
 #include <cstdlib>
