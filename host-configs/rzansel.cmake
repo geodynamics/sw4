ab02f94491babf166e51b2535debedcf279ade40
Ramesh Pankajakshan <pankajakshan1@llnl.gov>
2021-07-08 17:36:51 -0700

    Attempt to add Umpire to cmake build. Cub issues probably due to mismatch with Raja

--- host-configs/rzansel.cmake
+++ host-configs/rzansel.cmake
@@ -1,7 +1,9 @@
 set (LAPACK_DIR "/usr/tcetmp/packages/lapack/lapack-3.9.0-gcc-7.3.1" CACHE STRING "")
 set (PROJ4_DIR "/usr/workspace/wsrzd/ramesh/Project6/2021/SCALING/NEWCODE/sw4_cmake/tpl/PROJ/install" CACHE STRING "")
+#set (UMPIRE_DIR "/usr/workspace/wsrzd/ramesh/UMPIRE/umpire-3.0.0" CACHE STRING "")
 set (ENABLE_MPI ON CACHE BOOL "" FORCE)
 set (ENABLE_CUDA ON CACHE BOOL "" FORCE)
+set (RNABLE_OPENMP OFF CACHE BOOL "" FORCE)
 set (ENABLE_RAJA ON CACHE BOOL "" FORCE)
 
 
