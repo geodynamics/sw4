dfbf458352558f56f3a7a813589d1cfddf00a4e3
Ramesh Pankajakshan <pankajakshan1@llnl.gov>
2021-09-16 12:49:57 -0700

    optional SF version works with HIP. Not tested with rocm4.3. HMR3 is faster(7:38:vs 7:13)=5%

--- src/hip_foralls.h
+++ src/hip_foralls.h
@@ -644,4 +644,44 @@ void gmforall3async(T &start, T &end, LoopBody &&body, Args... args) {
   gmforallkernel3<<<blocks, tpb>>>(start, end, body, args...);
 }
 
+// Split Fusion kernels
+template <int N, typename Tag, typename... Func>
+  __launch_bounds__(256,1)
+__global__ void forall3kernelSF(Tag t, const int start0, const int N0,
+                              const int start1, const int N1, const int start2,
+                              const int N2, Func... f) {
+  const int STORE=5;
+#define USE_SHARED_MEMORY 1
+#ifdef USE_SHARED_MEMORY
+  int off = (threadIdx.x+blockDim.x*threadIdx.y+blockDim.x*blockDim.y*threadIdx.z)*STORE;
+  __shared__ double sma[512*STORE];
+
+  double *carray = sma+off;
+#else
+  double carray[STORE];
+
+#endif
+  //double sma2[3];
+
+  int tid0 = start0 + threadIdx.x + blockIdx.x * blockDim.x;
+  int tid1 = start1 + threadIdx.y + blockIdx.y * blockDim.y;
+  int tid2 = start2 + threadIdx.z + blockIdx.z * blockDim.z;
+  if ((tid0 < N0) && (tid1 < N1) && (tid2 < N2)) {
+    (f(t, carray,tid0, tid1, tid2),...); 
+  }
+}
+
+template <int N, typename Tag, typename T1, typename T2, typename T3,
+  typename... LoopBodies>
+  void forall3asyncSF(Tag &t, T1 &irange, T2 &jrange, T3 &krange, LoopBodies &&... bodies) {
+  
+  if (irange.invalid || jrange.invalid || krange.invalid) { std::cerr<<"Invalid ranges in forall3asyncSF \n";return;}
+  dim3 tpb(irange.tpb, jrange.tpb, krange.tpb);
+  dim3 blocks(irange.blocks, jrange.blocks, krange.blocks);
+
+  hipLaunchKernelGGL(forall3kernelSF<N>, blocks, tpb, 0, 0, t, irange.start, irange.end, jrange.start,
+  				     jrange.end, krange.start, krange.end, bodies...);
+
+}
+
 #endif  // Guards
