6477bbf36c43754d48caf257e4bf96f56434f59b
Sean Ford <ford17@llnl.gov>
2021-11-05 14:38:22 -0700

    geodynbc test reference input and output files

--- pytest/reference/geodynbc/makewhifile.py
+++ pytest/reference/geodynbc/makewhifile.py
@@ -7,11 +7,11 @@
 
 def main():
     fout = open("loh1-h100-mr.whi", "w")
-    n = 4
+    n = 11
     h = 50
     sx = 15000
     sy = 15000
-    sz = 100
+    sz = 250
     whidir = "loh1-h100-mr-whi/"
     steps = 256
     dt = 0.0132681
@@ -20,34 +20,34 @@ def main():
     text = []
     # Sides 1 & 2
     for s in range(1, 3):  # side
-        for k in range(n + 1):  # z
-            for j in range(n + 1):  # y
+        for k in range(n):  # z
+            for j in range(n):  # y
                 with open(whidir + "side%d_z%d_y%d.txt" % (s, k + 1, j + 1)) as f:
                     text.extend(f.readlines()[13 : (13 + steps)])
     # Sides 3 & 4
     for s in range(3, 5):
-        for k in range(n + 1):  # z
-            for i in range(n + 1):  # x
+        for k in range(n):  # z
+            for i in range(n):  # x
                 with open(whidir + "side%d_z%d_x%d.txt" % (s, k + 1, i + 1)) as f:
                     text.extend(f.readlines()[13 : (13 + steps)])
     # Sides 5 & 6
     for s in range(5, 7):
-        for j in range(n + 1):  # y
-            for i in range(n + 1):  # x
+        for j in range(n):  # y
+            for i in range(n):  # x
                 with open(whidir + "side%d_y%d_x%d.txt" % (s, j + 1, i + 1)) as f:
                     text.extend(f.readlines()[13 : (13 + steps)])
 
     # Write WHI file
     fout.write(
         "grid faces=6 stepsize=%d nx=%d ny=%d nz=%d x0=%d y0=%d z0=%d adjust=1\n"
-        % (h, n + 1, n + 1, n + 1, sx - (h * n / 2), sy - (h * n / 2), sz - (h * n / 2))
+        % (h, n, n, n, sx - (h * (n-1) / 2), sy - (h * (n-1) / 2), sz - (h * (n-1) / 2))
     )
     fout.write("time timestep=%f nsteps=%d\n" % (dt, steps))
     fout.write("begindata\n")
     # Step
     for t in range(steps):
         #
-        for l in range(6 * (n + 1) * (n + 1)):
+        for l in range(6 * n * n):
             # Just write out the x, y, z (no time in space-delimited column 1)
             fout.write(text[t + l * steps].partition(" ")[2])
 
--- pytest/reference/geodynbc/makewhireclines.py
+++ pytest/reference/geodynbc/makewhireclines.py
@@ -3,26 +3,27 @@
 # Make rec lines to create whi file input
 # e.g., rec x=15600 y=15800 z=0 file=sta01 usgsformat=1 sacformat=0
 # source x=15000 y=15000 z=2000 mxy=1e18 t0=0.36 freq=16.6667 type=Gaussian
-
+# n = number of nodes in box
+# h, sx, sy, sz are from loh1-h100-mr-whi.in file
 
 def main():
     f = open("whireclines.txt", "w")
-    n = 4
+    n = 11
     h = 50
     sx = 15000
     sy = 15000
-    sz = 100
+    sz = 250
 
     # Sides 1 & 2
     for s in range(2):  # side
-        for k in range(n + 1):  # z
-            for j in range(n + 1):  # y
+        for k in range(n):  # z
+            for j in range(n):  # y
                 f.write(
                     "rec x=%1d y=%1d z=%1d file=side%1d_z%1d_y%1d usgsformat=1 sacformat=0\n"
                     % (
-                        sx - (h * n / 2) + 2 * s * (h * n / 2),  # trick for sides
-                        sy - (h * n / 2) + (j * h),
-                        sz - (h * n / 2) + (k * h),
+                        sx - (h * (n-1) / 2) + 2 * s * (h * (n-1) / 2),  # trick for sides
+                        sy - (h * (n-1) / 2) + (j * h),
+                        sz - (h * (n-1) / 2) + (k * h),
                         s + 1,
                         k + 1,
                         j + 1,
@@ -31,14 +32,14 @@ def main():
 
     # Sides 3 & 4
     for s in range(2):  # side
-        for k in range(n + 1):  # z
-            for i in range(n + 1):  # x
+        for k in range(n):  # z
+            for i in range(n):  # x
                 f.write(
                     "rec x=%1d y=%1d z=%1d file=side%1d_z%1d_x%1d usgsformat=1 sacformat=0\n"
                     % (
-                        sx - (h * n / 2) + (i * h),
-                        sy - (h * n / 2) + 2 * s * (h * n / 2),  # trick for sides
-                        sz - (h * n / 2) + (k * h),
+                        sx - (h * (n-1) / 2) + (i * h),
+                        sy - (h * (n-1) / 2) + 2 * s * (h * (n-1) / 2),  # trick for sides
+                        sz - (h * (n-1) / 2) + (k * h),
                         s + 3,
                         k + 1,
                         i + 1,
@@ -47,14 +48,14 @@ def main():
 
     # Sides 5 & 6
     for s in range(2):  # side
-        for j in range(n + 1):  # y
-            for i in range(n + 1):  # x
+        for j in range(n):  # y
+            for i in range(n):  # x
                 f.write(
                     "rec x=%1d y=%1d z=%1d file=side%1d_y%1d_x%1d usgsformat=1 sacformat=0\n"
                     % (
-                        sx - (h * n / 2) + (i * h),
-                        sy - (h * n / 2) + (j * h),
-                        sz - (h * n / 2) + 2 * s * (h * n / 2),  # trick for sides,
+                        sx - (h * (n-1) / 2) + (i * h),
+                        sy - (h * (n-1) / 2) + (j * h),
+                        sz - (h * (n-1) / 2) + 2 * s * (h * (n-1) / 2),  # trick for sides,
                         s + 5,
                         j + 1,
                         i + 1,
