562c3965c9bc42d529eec53ff2e374b2bd62cca8
Houjun Tang <houj.tang@gmail.com>
2021-12-16 18:38:12 -0800

    Update dependencies-linux.sh

--- .github/workflows/dependencies-linux.sh
+++ .github/workflows/dependencies-linux.sh
@@ -4,4 +4,4 @@ set -eu -o pipefail
 
 sudo apt-get update
 
-sudo apt-get install proj-bin libhdf5-mpi-dev libfftw3-dev libfftw3-mpi-dev liblapack-dev python3-h5py curl
+sudo apt-get install libproj-dev libhdf5-mpi-dev libfftw3-dev libfftw3-mpi-dev liblapack-dev python3-h5py curl
