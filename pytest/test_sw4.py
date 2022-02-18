6f0afe52d229d70ee6c32f1a84905a210630e251
Ramesh Pankajakshan <pankajakshan1@llnl.gov>
2022-02-17 21:03:49 -0500

    Added env variable to pytest. Modified bind to take any number of arguments

--- pytest/test_sw4.py
+++ pytest/test_sw4.py
@@ -170,8 +170,9 @@ def guess_mpi_cmd(mpi_tasks, omp_threads, cpu_allocation, verbose):
         if mpi_tasks<=0: mpi_tasks = 4
         mpirun_cmd="srun -p amdMI100 -n1 "
     elif 'login' in node_name:
-        os.environ["PSM2_DEVICES"] = ""
-        os.environ["ROMIO_FSTYPE_FORCE"]="ufs:"
+        #os.environ["PSM2_DEVICES"] = ""
+        #os.environ["ROMIO_FSTYPE_FORCE"]="ufs:"
+        os.environ["MPICH_GPU_SUPPORT_ENABLED"]="1"
         if mpi_tasks<=0: mpi_tasks = 4
         mpirun_cmd="srun -N 1 -A GEO130_crusher -t 30 -n 8 -c8 --gpus-per-task=1 --gpu-bind=closest -p batch "
     elif 'lassen' in node_name:
