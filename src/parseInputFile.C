c8f17e420e0854eb65b0b0e293d918b4bf005e9a
Houjun Tang <htang4@lbl.gov>
2022-09-27 16:52:41 -0700

    Remove support for proj.4, use proj version 6 or later, CMake is now working

--- src/parseInputFile.C
+++ src/parseInputFile.C
@@ -1265,9 +1265,9 @@ void EW::processGrid(char* buffer)
   // hard code units to be in meters
   proj0 << " +units=m";
 
-#if !defined(ENABLE_PROJ4) && !defined(ENABLE_PROJ_6) 
+#if !defined(ENABLE_PROJ)
   CHECK_INPUT( !use_geoprojection, "ERROR: need to configure SW4 with proj=yes to use projections "
-               "from the Proj4 library");
+               "from the PROJ library (version 6 or later)");
 #endif
   if( use_geoprojection )
   {
