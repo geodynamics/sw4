49c7777293c5df50c79c80b39d179cb13641e2e1
Houjun Tang <htang4@lbl.gov>
2023-03-22 13:46:18 -0700

    remove redundent allocation

--- src/Qspline.C
+++ src/Qspline.C
@@ -198,7 +198,6 @@ Qspline::Qspline( int npts, float_sw4* fun, float_sw4 tmin, float_sw4 dt, int bc
    else
       m_polcof = new float_sw4[6];
 
-   m_polcof = new float_sw4[6*(npts-1)];
    for( int i= 0 ; i < npts-1 ; i++ )
    {
       m_polcof[  6*i] = fun[i];
