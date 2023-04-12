e4621e843cc5b910b897035df3a306686026b7f5
Ramesh Pankajakshan <pankajakshan1@llnl.gov>
2023-04-12 16:23:14 -0700

    Fixed deprecated feature in pytest

--- pytest/test_sw4.py
+++ pytest/test_sw4.py
@@ -183,7 +183,7 @@ def guess_mpi_cmd(mpi_tasks, omp_threads, cpu_allocation, verbose):
         if mpi_tasks<=0: mpi_tasks = 4
         mpirun_cmd="srun -N 1 -A GEO130_crusher -t 30 -n 8 -c8 --gpus-per-task=1 --gpu-bind=closest -p batch "
     elif 'rzvernal' in node_name:
-        os.environ["MPICH_GPU_SUPPORT_ENABLED"]="1"
+        #os.environ["MPICH_GPU_SUPPORT_ENABLED"]="1"
         if mpi_tasks<=0: mpi_tasks = 4
         mpirun_cmd="srun -N 1 -n 8 -c8 --gpus-per-task=1 --gpu-bind=closest "
     elif 'tioga' in node_name:
