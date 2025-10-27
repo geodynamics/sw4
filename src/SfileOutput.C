05f926adbde96cd925b80cd64a97a52dc5d745e7
Houjun Tang <htang4@lbl.gov>
2025-10-27 11:24:21 -0700

    Fix sfile output

--- src/SfileOutput.C
+++ src/SfileOutput.C
@@ -740,7 +740,7 @@ void SfileOutput::write_image(const char *fname, std::vector<Sarray>& a_Z )
     H5Aclose(attr);
 
     aname = "Coarsest horizontal grid spacing";
-    double spacing = mEW->mGridSize[0]*stH;
+    spacing = mEW->mGridSize[0]*stH;
     attr = H5Acreate(h5_fid, aname, H5T_NATIVE_DOUBLE, attr_space1, H5P_DEFAULT, H5P_DEFAULT);
     if( attr < 0 )
       VERIFY2(0, "ERROR: SfileOutput::write_image, error creating " << aname);
