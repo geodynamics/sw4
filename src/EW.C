1b15aaea405dae46fd3e4825744bf8ee0038e7e1
Houjun Tang <htang4@lbl.gov>
2026-06-01 16:09:01 -0700

    Fix random material redistribution communicator

--- src/EW.C
+++ src/EW.C
@@ -8903,7 +8903,8 @@ AllDims* EW::get_fine_alldimobject() {
   int g = mNumberOfCartesianGrids - 1;
   AllDims* fine = new AllDims(m_proc_array[0], m_proc_array[1], 1, 1,
                               m_global_nx[g], 1, m_global_ny[g], 1,
-                              m_global_nz[g], m_ghost_points, m_ppadding);
+                              m_global_nz[g], m_ghost_points, m_ppadding,
+                              m_cartesian_communicator);
   return fine;
 }
 //-----------------------------------------------------------------------
