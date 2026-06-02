0b0ea91918a376ee162c88027a33c3604a8d7fed
Houjun Tang <houj.tang@gmail.com>
2026-06-02 00:08:00 -0400

    Fix SAC HDF5 file open race

--- src/TimeSeries.C
+++ src/TimeSeries.C
@@ -4061,7 +4061,8 @@ hid_t TimeSeries::openHDF5File(std::string suffix)
   if (m_hdf5Name.find(".hdf5") == string::npos && m_hdf5Name.find(".h5") == string::npos) 
     filename.append(".hdf5");
 
-  if (*m_fid_ptr >=0 && this->m_ts0Ptr && filename.compare(this->m_ts0Ptr->m_fidName) == 0) {
+  if (*m_fid_ptr > 0 && this->m_ts0Ptr &&
+      filename.compare(this->m_ts0Ptr->m_fidName) == 0) {
     // If file is alread open, no need to open it again
     return *m_fid_ptr;
   }
